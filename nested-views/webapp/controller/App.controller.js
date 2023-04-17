3edd4126c49c60c24d494df28392231b0e7a728a
Sameer Sarmah <s.sarmah@sap.com>
2023-04-17 15:01:46 +0530

    nested view dynamically

--- nested-views/webapp/controller/App.controller.js
+++ nested-views/webapp/controller/App.controller.js
@@ -1,12 +1,24 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller","sap/ui/core/mvc/XMLView"
+], function (Controller,XMLView) {
 	"use strict";
 
 	return Controller.extend("sap.ui.demo.controller.App", {
 
 		onInit: function () {
-			//this.getView().placeAt("content");
+			const view = this.getView();
+			const helloViewPromise =  XMLView.create({
+				viewName: "sap.ui.demo.view.HelloPanel"
+			});
+			const productViewPromise =  XMLView.create({
+				viewName: "sap.ui.demo.view.Product"
+			});
+			$.when(helloViewPromise, productViewPromise).done(function (helloView,productView) {
+				const page = view.byId("appPage");
+				page.addContent(helloView);
+				page.addContent(productView);
+			});
+
 		}
 	});
 
